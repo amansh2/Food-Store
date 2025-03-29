c62af50e9693bb8aa1611a3b7415749f9c36e7d1
amandeep <amanshdeep2@gmail.com>
2025-03-29 20:37:54 +0530

    cors fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,8 +12,20 @@ dbConnect();
 app.use(express.json());
 app.use(cors({
     credentials:true,
-    origin:["http://localhost:4200"] 
+    origin:["https://food-store-l2fx.onrender.com", "http://localhost:4200", 'https://savorstreet.netlify.app'] 
 }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(204);  // Ends the request-response cycle for preflight requests
+    }
+
+    next();  // Pass control to the next middleware or route handler
+});
 app.use('/api/foods',foodRouter)
 app.use('/api/users',userRouter)
 app.use('/api/orders', orderRouter)
